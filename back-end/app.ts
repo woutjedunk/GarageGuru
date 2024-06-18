0df0a83226f8c3d27f3b87c67d703e04e689f02e
woutjedunk <61290530+woutjedunk@users.noreply.github.com>
2024-06-18 14:40:25 +0200

    redid app to enable awaits in app.ts

--- back-end/app.ts
+++ back-end/app.ts
@@ -10,95 +10,107 @@ import { Request, Response, NextFunction } from 'express';
 // import helmet from 'helmet';
 
 import carRouter from './controller/car.routes';
-
-
-const app = express();
-dotenv.config();
-// if (!process.env.JWT_SECRET) {
-//     throw new Error('JWT_SECRET must be defined')
-// }
-const port = process.env.APP_PORT || 3000;
-
-
-app.use(cors());
-app.use(bodyParser.json());
-
-// app.use(
-//     expressjwt({
-//         secret: process.env.JWT_SECRET!,
-//         algorithms: ['HS256'],
-//     }).unless({
-//         path: [
-//             '/api-docs',
-//             /^\/api-docs\/.*/,
-//             '/users/signup',
-//             '/users/login',
-//             /^\/users\/email\/*/,
-//             '/status',
-//         ],
-//     })
-// );
+import { Tspec, TspecDocsMiddleware } from 'tspec';
 
 
 
-// app.use('/users', userRouter);
-app.use("/car", carRouter)
 
 
-app.get('/status', (req, res) => {
-    res.json({ message: 'Back-end is running...' });
-});
 
-// const swaggerOpts = {
-//     definition: {
-//         openapi: '3.0.0',
-//         info: {
-//             title: 'MyVehicleVault API',
-//             version: '1.0.0',
-//         },
-//     },
-//     apis: ['./controller/*.routes.ts'],
-// };
-// const swaggerSpec = swaggerJsdoc(swaggerOpts);
-
-// app.use(helmet())
-
-// app.use(
-//     helmet.contentSecurityPolicy({
-//         directives: {
-//             connectSrc: ["'self'"],
-//         },
-//     })
-// )
-
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+const initServer = async () => {
+    const app = express();
+    dotenv.config();
+    // if (!process.env.JWT_SECRET) {
+    //     throw new Error('JWT_SECRET must be defined')
+    // }
+    const port = process.env.APP_PORT || 3000;
 
 
-// error hanling
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    switch (err.name) {
-        case 'UnauthorizedError':
-            res.status(401).json({
-                status: 'application error',
-                message: err.message,
-            });
-            break;
-            
-        case 'ResourceNotFoundError':
-            res.status(404).json({
-                status: 'application error',
-                message: err.message,
-            })
+    app.use(cors());
+    app.use(bodyParser.json());
 
-        default:
-            res.status(400).json({
-                status: 'application error',
-                message: err.message,
-            });
-            break;
-    }
-});
+    // app.use(
+    //     expressjwt({
+    //         secret: process.env.JWT_SECRET!,
+    //         algorithms: ['HS256'],
+    //     }).unless({
+    //         path: [
+    //             '/api-docs',
+    //             /^\/api-docs\/.*/,
+    //             '/users/signup',
+    //             '/users/login',
+    //             /^\/users\/email\/*/,
+    //             '/status',
+    //         ],
+    //     })
+    // );
 
-app.listen(port || 3000, () => {
-    console.log(`Back-end is running on port ${port}.`);
-});
+
+
+    // app.use('/users', userRouter);
+    app.use("/car", carRouter)
+
+
+
+    app.use('/docs', await TspecDocsMiddleware());
+
+    app.get('/status', (req, res) => {
+        res.json({ message: 'Back-end is running...' });
+    });
+
+    // const swaggerOpts = {
+    //     definition: {
+    //         openapi: '3.0.0',
+    //         info: {
+    //             title: 'MyVehicleVault API',
+    //             version: '1.0.0',
+    //         },
+    //     },
+    //     apis: ['./controller/*.routes.ts'],
+    // };
+    // const swaggerSpec = swaggerJsdoc(swaggerOpts);
+
+    // app.use(helmet())
+
+    // app.use(
+    //     helmet.contentSecurityPolicy({
+    //         directives: {
+    //             connectSrc: ["'self'"],
+    //         },
+    //     })
+    // )
+
+    // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+    // error hanling
+    app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+        switch (err.name) {
+            case 'UnauthorizedError':
+                res.status(401).json({
+                    status: 'application error',
+                    message: err.message,
+                });
+                break;
+
+            case 'ResourceNotFoundError':
+                res.status(404).json({
+                    status: 'application error',
+                    message: err.message,
+                })
+
+            default:
+                res.status(400).json({
+                    status: 'application error',
+                    message: err.message,
+                });
+                break;
+        }
+    });
+
+    app.listen(port || 3000, () => {
+        console.log(`Back-end is running on port ${port}.`);
+    });
+}
+
+initServer();
